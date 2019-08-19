59263b1fdd26ac07c306fe484808d02367cc5539
bubulbobul <windam@live.fr>
2019-08-19 10:56:58 +0530

    Updates

---
+++ precache-manifest.60c114387c94521f52a5841fd1c5af2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14fb379a9549f258e94afcbc2751e474",
+    "url": "/todo-app-with-redux/index.html"
+  },
+  {
+    "revision": "6299ddffffc227e0568e",
+    "url": "/todo-app-with-redux/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0c2e03170d67fd129c74",
+    "url": "/todo-app-with-redux/static/js/2.a0af5dbf.chunk.js"
+  },
+  {
+    "revision": "6299ddffffc227e0568e",
+    "url": "/todo-app-with-redux/static/js/main.a20c102d.chunk.js"
+  },
+  {
+    "revision": "a0dd3857ed2fee74c69d",
+    "url": "/todo-app-with-redux/static/js/runtime~main.12f6794d.js"
+  }
+]);
